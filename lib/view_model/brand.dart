b76a8fc8b9fa29a5d797c88eae87811c5be5af56
esraaabdrabo <esraaabdrabo23@gmail.com>
2022-07-28 14:20:31 +0200

    finish brandsVM

--- lib/view_model/brand.dart
+++ lib/view_model/brand.dart
@@ -1,3 +1,28 @@
 import 'package:flutter/cupertino.dart';
+import 'package:store/models/product.dart';
+import 'package:store/services/brand.dart';
 
-class BrandVM extends ChangeNotifier {}
+class BrandVM extends ChangeNotifier {
+  List<Product> products = [];
+  bool isLoading = false;
+  String errorMessage = '';
+
+  BrandVM(String brandName) {
+    getProducts(brandName);
+  }
+  changeLoading(bool value) {
+    isLoading = value;
+    notifyListeners();
+  }
+
+  getProducts(String brandName) async {
+    changeLoading(true);
+    var res = await BrandSV.getProductsBrand(brandName);
+    if (res is Products) {
+      products = res.brandsProducts;
+    } else {
+      errorMessage = res;
+    }
+    changeLoading(false);
+  }
+}
